836f93bb3035d09b0740b53d13264cbb343de1f2
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2014-02-27 15:19:15 +0100

    added contextual output

--- src/main/java/org/aksw/hawk/predicateArgumentTree/demoMultiParseTree.java
+++ src/main/java/org/aksw/hawk/predicateArgumentTree/demoMultiParseTree.java
@@ -46,6 +46,8 @@ import java.util.List;
 
 import com.clearnlp.component.AbstractComponent;
 import com.clearnlp.component.dep.AbstractDEPParser;
+import com.clearnlp.dependency.DEPArc;
+import com.clearnlp.dependency.DEPNode;
 import com.clearnlp.dependency.DEPTree;
 import com.clearnlp.nlp.NLPGetter;
 import com.clearnlp.nlp.NLPMode;
@@ -60,94 +62,111 @@ import com.clearnlp.util.pair.ObjectDoublePair;
  * @since 1.1.0
  * @author Jinho D. Choi ({@code jdchoi77@gmail.com})
  */
-public class demoMultiParseTree
-{
+public class demoMultiParseTree {
 	final String language = AbstractReader.LANG_EN;
-	
-	public demoMultiParseTree(String modelType, String inputFile, String outputFile) throws Exception
-	{
-		AbstractTokenizer tokenizer  = NLPGetter.getTokenizer(language);
-		AbstractComponent tagger     = NLPGetter.getComponent(modelType, language, NLPMode.MODE_POS);
-		AbstractComponent parser     = NLPGetter.getComponent(modelType, language, NLPMode.MODE_DEP);
+
+	public demoMultiParseTree(String modelType, String inputFile, String outputFile) throws Exception {
+		AbstractTokenizer tokenizer = NLPGetter.getTokenizer(language);
+		AbstractComponent tagger = NLPGetter.getComponent(modelType, language, NLPMode.MODE_POS);
+		AbstractComponent parser = NLPGetter.getComponent(modelType, language, NLPMode.MODE_DEP);
 		AbstractComponent identifier = NLPGetter.getComponent(modelType, language, NLPMode.MODE_PRED);
 		AbstractComponent classifier = NLPGetter.getComponent(modelType, language, NLPMode.MODE_ROLE);
-		AbstractComponent labeler    = NLPGetter.getComponent(modelType, language, NLPMode.MODE_SRL);
-		
-		AbstractComponent[] preComponents  = {tagger};	// components used before parsing
-		AbstractComponent[] postComponents = {identifier, classifier, labeler};	// components used after parsing
-		
+		AbstractComponent labeler = NLPGetter.getComponent(modelType, language, NLPMode.MODE_SRL);
+
+		AbstractComponent[] preComponents = { tagger }; // components used
+														// before parsing
+		AbstractComponent[] postComponents = { identifier, classifier, labeler }; // components
+																					// used
+																					// after
+																					// parsing
+
 		String sentence = "I know you know who I know.";
-		process(tokenizer, (AbstractDEPParser)parser, preComponents, postComponents, sentence);
-		process(tokenizer, (AbstractDEPParser)parser, preComponents, postComponents, UTInput.createBufferedFileReader(inputFile), UTOutput.createPrintBufferedFileStream(outputFile));
+		// process(tokenizer, (AbstractDEPParser) parser, preComponents,
+		// postComponents, sentence);
+		process(tokenizer, (AbstractDEPParser) parser, preComponents, postComponents, UTInput.createBufferedFileReader(inputFile), UTOutput.createPrintBufferedFileStream(outputFile));
 	}
-	
-	public void process(AbstractTokenizer tokenizer, AbstractDEPParser parser, AbstractComponent[] preComponents, AbstractComponent[] postComponents, String sentence)
-	{
+
+	public void process(AbstractTokenizer tokenizer, AbstractDEPParser parser, AbstractComponent[] preComponents, AbstractComponent[] postComponents, String sentence) {
 		DEPTree tree = NLPGetter.toDEPTree(tokenizer.getTokens(sentence));
 		List<ObjectDoublePair<DEPTree>> trees = getParses(parser, preComponents, postComponents, tree);
-		
-		for (ObjectDoublePair<DEPTree> p : trees)
-		{
-			tree = (DEPTree)p.o;
-			System.out.println("Score: "+p.d);
-			System.out.println(tree.toStringSRL()+"\n");
+
+		for (ObjectDoublePair<DEPTree> p : trees) {
+			tree = (DEPTree) p.o;
+			System.out.println("Score: " + p.d);
+			// System.out.println(tree.toStringSRL() + "\n");
+			System.out.println(tree.toStringCoNLL());
 		}
 	}
-	
-	public void process(AbstractTokenizer tokenizer, AbstractDEPParser parser, AbstractComponent[] preComponents, AbstractComponent[] postComponents, BufferedReader reader, PrintStream fout)
-	{
+
+	public void process(AbstractTokenizer tokenizer, AbstractDEPParser parser, AbstractComponent[] preComponents, AbstractComponent[] postComponents, BufferedReader reader, PrintStream fout) {
 		AbstractSegmenter segmenter = NLPGetter.getSegmenter(language, tokenizer);
 		List<ObjectDoublePair<DEPTree>> trees;
 		DEPTree tree;
-		
-		for (List<String> tokens : segmenter.getSentences(reader))
-		{
-			tree  = NLPGetter.toDEPTree(tokens);
+
+		for (List<String> tokens : segmenter.getSentences(reader)) {
+			tree = NLPGetter.toDEPTree(tokens);
 			trees = getParses(parser, preComponents, postComponents, tree);
-			
-			for (ObjectDoublePair<DEPTree> p : trees)
-			{
-				tree = (DEPTree)p.o;
-				fout.println("Score: "+p.d);
-				fout.println(tree.toStringSRL()+"\n");
+
+			for (ObjectDoublePair<DEPTree> p : trees) {
+				tree = (DEPTree) p.o;
+				fout.println("Score: " + p.d);
+				inorderTraversal(tree.getFirstRoot(), 0);
+				fout.println(tree.toStringSRL() + "\n");
 			}
 		}
-		
+
 		fout.close();
 	}
-	
-	private List<ObjectDoublePair<DEPTree>> getParses(AbstractDEPParser parser, AbstractComponent[] preComponents, AbstractComponent[] postComponents, DEPTree tree)
-	{
+
+	private void inorderTraversal(DEPNode depNode, int i) {
+		System.out.println(printTabs(i) + depNode.lemma );
+		++i;
+		for (DEPNode node : depNode.getDependentNodeList()) {
+			inorderTraversal(node, i);
+		}
+	}
+
+	private String printTabs(int i) {
+		String tabs = "";
+		if (i > 0) {
+			tabs = "|";
+		}
+		for (int j = 0; j < i; ++j) {
+			tabs += "=";
+		}
+		tabs+=">";
+		return tabs;
+	}
+
+	private List<ObjectDoublePair<DEPTree>> getParses(AbstractDEPParser parser, AbstractComponent[] preComponents, AbstractComponent[] postComponents, DEPTree tree) {
 		List<ObjectDoublePair<DEPTree>> trees;
-		boolean uniqueOnly = true;	// return only unique trees given a sentence 
-		
+		boolean uniqueOnly = true; // return only unique trees given a sentence
+
 		for (AbstractComponent component : preComponents)
 			component.process(tree);
-		
+
 		trees = parser.getParsedTrees(tree, uniqueOnly);
-		
+
 		// parses are already sorted by their scores in descending order
-		for (ObjectDoublePair<DEPTree> p : trees)
-		{
-			tree = (DEPTree)p.o;
-			
+		for (ObjectDoublePair<DEPTree> p : trees) {
+			tree = (DEPTree) p.o;
+
 			for (AbstractComponent component : postComponents)
 				component.process(tree);
 		}
-		
+
 		return trees;
 	}
 
-	public static void main(String[] args)
-	{
-		String modelType  = args[0];	// "general-en" or "medical-en"
-		String inputFile  = args[1];
+	public static void main(String[] args) {
+		String modelType = args[0]; // "general-en" or "medical-en"
+		String inputFile = args[1];
 		String outputFile = args[2];
 
-		try
-		{
+		try {
 			new demoMultiParseTree(modelType, inputFile, outputFile);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		catch (Exception e) {e.printStackTrace();}
 	}
 }
