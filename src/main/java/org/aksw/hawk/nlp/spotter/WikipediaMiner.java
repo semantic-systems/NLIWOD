ee7fdc030e2a9899db92af993787cd4650f6f8ec
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2014-05-02 10:09:13 +0200

    pushed debugging deeper and thus correcter

--- src/main/java/org/aksw/hawk/nlp/spotter/WikipediaMiner.java
+++ src/main/java/org/aksw/hawk/nlp/spotter/WikipediaMiner.java
@@ -21,6 +21,7 @@ import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Joiner;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.impl.ResourceImpl;
 
@@ -101,6 +102,9 @@ public class WikipediaMiner extends ASpotter {
 
 		} catch (IOException | ParseException e) {
 			log.error(e.getLocalizedMessage(), e);
+		}
+		if (!tmp.isEmpty()) {
+			log.debug("\t" + Joiner.on("\n").join(tmp.get("en")));
 		}
 		return tmp;
 	}
