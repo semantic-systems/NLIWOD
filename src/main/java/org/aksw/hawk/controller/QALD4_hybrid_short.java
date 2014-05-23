e4c94896e96d4b3fa4798a32dcd9cf23766d90e4
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2014-05-23 16:37:51 +0200

    added cache data and a recall 1 creator via simply asking the index

---
+++ src/main/java/org/aksw/hawk/controller/QALD4_hybrid_short.java
@@ -0,0 +1,44 @@
+package org.aksw.hawk.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.aksw.autosparql.commons.qald.Qald4HybridLoader;
+import org.aksw.hawk.module.ModuleBuilder;
+import org.aksw.hawk.module.PseudoQueryBuilder;
+import org.aksw.hawk.module.SystemAnswerer;
+import org.aksw.hawk.nlp.Pruner;
+import org.aksw.hawk.nlp.SentenceToSequence;
+import org.aksw.hawk.nlp.spotter.Fox;
+import org.aksw.hawk.nlp.util.CachedParseTree;
+import org.aksw.hawk.pruner.GraphNonSCCPruner;
+import org.aksw.hawk.pruner.QueryVariableHomomorphPruner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QALD4_hybrid_short {
+	static Logger log = LoggerFactory.getLogger(QALD4_hybrid_short.class);
+
+	public static void main(String args[]) throws IOException {
+		ShortRecallPipeline controller = new ShortRecallPipeline();
+
+		log.info("Configuring controller");
+
+		controller.dataset = new File(args[0]).getAbsolutePath();
+		controller.datasetLoader = new Qald4HybridLoader();
+		controller.nerdModule = new Fox();
+		controller.cParseTree = new CachedParseTree();
+		controller.sentenceToSequence = new SentenceToSequence();
+		controller.pruner = new Pruner();
+		controller.moduleBuilder = new ModuleBuilder();
+		controller.pseudoQueryBuilder = new PseudoQueryBuilder();
+		controller.queryVariableHomomorphPruner = new QueryVariableHomomorphPruner();
+		controller.graphNonSCCPruner = new GraphNonSCCPruner();
+		String endpoint = "http://dbpedia.org/sparql";
+		controller.systemAnswerer = new SystemAnswerer(endpoint, controller.nerdModule);
+
+		log.info("Run controller");
+		controller.run();
+
+	}
+}
--- src/main/java/org/aksw/hawk/controller/ShortRecallPipeline.java
+++ src/main/java/org/aksw/hawk/controller/ShortRecallPipeline.java
@@ -2,18 +2,20 @@ package org.aksw.hawk.controller;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 
 import org.aksw.autosparql.commons.qald.QALD4_EvaluationUtils;
 import org.aksw.autosparql.commons.qald.Qald4HybridLoader;
 import org.aksw.autosparql.commons.qald.Question;
+import org.aksw.hawk.index.DBAbstractsIndex;
 import org.aksw.hawk.module.ModuleBuilder;
 import org.aksw.hawk.module.PseudoQueryBuilder;
 import org.aksw.hawk.module.SystemAnswerer;
 import org.aksw.hawk.nlp.Pruner;
 import org.aksw.hawk.nlp.SentenceToSequence;
+import org.aksw.hawk.nlp.posTree.MutableTreeNode;
 import org.aksw.hawk.nlp.spotter.ASpotter;
 import org.aksw.hawk.nlp.util.CachedParseTree;
 import org.aksw.hawk.pruner.GraphNonSCCPruner;
@@ -21,11 +23,13 @@ import org.aksw.hawk.pruner.QueryVariableHomomorphPruner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.hp.hpl.jena.query.ParameterizedSparqlString;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.impl.ResourceImpl;
 
-public class PipelineController {
-	static Logger log = LoggerFactory.getLogger(PipelineController.class);
+public class ShortRecallPipeline {
+	static Logger log = LoggerFactory.getLogger(ShortRecallPipeline.class);
 	String dataset;
 	Qald4HybridLoader datasetLoader;
 	ASpotter nerdModule;
@@ -50,54 +54,48 @@ public class PipelineController {
 
 			// 3. Build trees from questions and cache them
 			q.tree = cParseTree.process(q);
-			log.info(q.tree.toString());
 			// noun combiner, so the number of nodes in the DEPTree decreases
-			// nounCombiner.combineNouns(q);
 			sentenceToSequence.combineSequences(q);
-			// visualize the tree
-			vis.visTree(q);
 
 			// 4. Apply pruning rules
 			q.tree = pruner.prune(q);
-
-			// visualize the tree
-			vis.vis(q, nerdModule);
-			// 5. Build modules
-			q.modules = this.moduleBuilder.build(q);
-
-			// 8. Build pseudo queries
-			Iterator<ParameterizedSparqlString> iter = pseudoQueryBuilder.buildQuery(q);
-			log.info("Built PseudoQueries");
-			queryVariableHomomorphPruner.reset();
-			while (iter.hasNext()) {
-				ParameterizedSparqlString thisQuery = iter.next();
-				if (thisQuery != null) {
-					// check whether clauses are connected
-					if (graphNonSCCPruner.isSCC(thisQuery)) {
-						// homogenize variables in queries
-						if (queryVariableHomomorphPruner.queryHasNotBeenHandled(thisQuery)) {
-							// 10. Execute queries to generate system answers
-							HashMap<String, Set<RDFNode>> answer = systemAnswerer.answer(thisQuery);
-							for (String key : answer.keySet()) {
-								Set<RDFNode> systemAnswers = answer.get(key);
-								// 11. Compare to set of resources from
-								// benchmark
-								double precision = QALD4_EvaluationUtils.precision(systemAnswers, q);
-								double recall = QALD4_EvaluationUtils.recall(systemAnswers, q);
-								double fMeasure = QALD4_EvaluationUtils.fMeasure(systemAnswers, q);
-								if (fMeasure > 0) {
-									log.info("\tP=" + precision + " R=" + recall + " F=" + fMeasure);
-									log.info(key);
-								}
-							}
-						}
-					}
+			log.info(q.tree.toString());
+			
+			HashMap<String, Set<RDFNode>> answer = fulltext(q);
+			for (String key : answer.keySet()) {
+				Set<RDFNode> systemAnswers = answer.get(key);
+				// 11. Compare to set of resources from
+				// benchmark
+				double precision = QALD4_EvaluationUtils.precision(systemAnswers, q);
+				double recall = QALD4_EvaluationUtils.recall(systemAnswers, q);
+				double fMeasure = QALD4_EvaluationUtils.fMeasure(systemAnswers, q);
+				if (fMeasure > 0) {
+					log.info("\tP=" + precision + " R=" + recall + " F=" + fMeasure);
+					log.info(key);
 				}
 			}
-			vis.horRule();
-			System.gc();
-			break;
 		}
-		vis.close();
+	}
+
+	private HashMap<String, Set<RDFNode>> fulltext(Question q) {
+		HashMap<String, Set<RDFNode>> map = Maps.newHashMap();
+		DBAbstractsIndex index = new DBAbstractsIndex();
+		Set<RDFNode> set = Sets.newHashSet();
+		Stack<MutableTreeNode> stack = new Stack<MutableTreeNode>();
+		stack.push(q.tree.getRoot());
+		while (!stack.isEmpty()) {
+			MutableTreeNode tmp = stack.pop();
+			if (tmp.posTag.equals("CombinedNN")) {
+				for (String resourceURL : index.listAbstractsContaining(tmp.label)) {
+					set.add(new ResourceImpl(resourceURL));
+				}
+			}
+			for (MutableTreeNode child : tmp.getChildren()) {
+				stack.push(child);
+			}
+		}
+		map.put(q.languageToQuestion.get("en"), set);
+		index.close();
+		return map;
 	}
 }
