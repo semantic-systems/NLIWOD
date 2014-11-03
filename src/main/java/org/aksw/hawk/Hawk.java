7d876b0adc53badcaf4a52acb2add637803a8202
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2014-11-03 09:32:14 +0100

    fixed bug resulted from automatic merging

--- src/main/java/org/aksw/hawk/Hawk.java
+++ src/main/java/org/aksw/hawk/Hawk.java
@@ -3,19 +3,15 @@
  */
 package org.aksw.hawk;
 
-import java.io.File;
 import java.util.Map;
 import java.util.Set;
 
-import org.aksw.autosparql.commons.qald.QALD_Loader;
 import org.aksw.autosparql.commons.qald.Question;
 import org.aksw.hawk.cache.AbstractIndexCache;
 import org.aksw.hawk.cache.CachedParseTree;
 import org.aksw.hawk.controller.Pipeline;
-import org.aksw.hawk.index.DBAbstractsIndex;
 import org.aksw.hawk.nlp.SentenceToSequence;
 import org.aksw.hawk.nlp.spotter.Fox;
-import org.aksw.hawk.nlp.spotter.Spotlight;
 import org.aksw.hawk.pruner.Pruner;
 import org.aksw.hawk.querybuilding.Annotater;
 import org.aksw.hawk.querybuilding.SPARQLQueryBuilder;
@@ -35,10 +31,9 @@ public class Hawk {
 		controller.cParseTree = new CachedParseTree();
 
 		AbstractIndexCache cache = new AbstractIndexCache();
-		DBAbstractsIndex index = new DBAbstractsIndex(cache);
-		controller.sentenceToSequence = new SentenceToSequence(index);
-		controller.queryBuilder = new SPARQLQueryBuilder(index);
-		controller.annotater = new Annotater(index);
+		controller.sentenceToSequence = new SentenceToSequence();
+		controller.queryBuilder = new SPARQLQueryBuilder();
+		controller.annotater = new Annotater();
 
 		controller.pruner = new Pruner();
 		
