b7e910ade0513f4229478d8dd998d1602156741d
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2015-01-07 13:09:31 +0100

    Added pruners and improved f-measure  * pruning now underdefined queries e.g. having too less constraints  * pruning now queries havng too many predicates per edge  * added tests  * 12:11:11,533 INFO [org.aksw.hawk.controller.Pipeline.run] 92 - <Average P=0.6822638146167558 R=0.7941176470588235 F=0.6840120663650077 Counter=17.0>

---
+++ src/test/java/hawk/SpecificQueryTest.java
@@ -0,0 +1,43 @@
+package hawk;
+
+import java.util.Set;
+
+import org.aksw.hawk.pruner.SPARQLQueryPruner;
+import org.aksw.hawk.querybuilding.SPARQL;
+import org.aksw.hawk.querybuilding.SPARQLQuery;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Sets;
+
+public class SpecificQueryTest {
+	Logger log = LoggerFactory.getLogger(SpecificQueryTest.class);
+	SPARQL sparql = new SPARQL();
+	SPARQLQueryPruner pruner = new SPARQLQueryPruner(sparql);
+
+	@Test
+	public void specificQuery() {
+
+		// PREFIX text: <http://jena.apache.org/text#>
+		// SELECT DISTINCT ?proj WHERE {
+		// ?proj text:query (<http://dbpedia.org/ontology/abstract> '"Coquette
+		// Productions"' 1000).
+		// ?const <http://dbpedia.org/ontology/starring> ?proj.
+		// }
+		// LIMIT 12
+		Set<SPARQLQuery> queries = Sets.newHashSet();
+		SPARQLQuery query = new SPARQLQuery("?const <http://dbpedia.org/ontology/starring> ?proj.");
+		query.addFilterOverAbstractsContraint("?proj", "Coquette Productions");
+		log.debug(query.toString());
+		queries.add(query);
+
+		log.debug("Size before pruning: " + queries.size());
+		queries = pruner.prune(queries);
+		log.debug("Size after pruning: " + queries.size());
+		Assert.assertTrue(queries.size() == 1);
+
+	}
+
+}
