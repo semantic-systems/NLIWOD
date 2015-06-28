0bc72a8de717f6cbbea9b23d048d28bc455fefed
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2015-06-28 12:55:03 +0200

    Async call to slow pipeline with status json emitted to the front. That was pretty easy.

--- src/main/java/org/aksw/hawk/webservice/WebController.java
+++ src/main/java/org/aksw/hawk/webservice/WebController.java
@@ -1,29 +1,35 @@
 package org.aksw.hawk.webservice;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import org.aksw.autosparql.commons.qald.Question;
-import org.aksw.hawk.controller.Answer;
-import org.aksw.hawk.controller.Pipeline;
-import org.aksw.hawk.ranking.BucketRanker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.util.SimpleIdGenerator;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.common.base.Joiner;
 import com.google.common.collect.Maps;
 
 @RestController
+@EnableAsync
 public class WebController {
-	Logger log = LoggerFactory.getLogger(WebController.class);
-	private Pipeline pipeline = new Pipeline();
+
+	@Autowired
+	@Qualifier("asyncSearchExecutor")
+	private AsyncSearchExecutor asyncSearchExecutor;
+
+	private Logger log = LoggerFactory.getLogger(WebController.class);
+	private HashMap<UUID, Future<Question>> runningProcesses = Maps.newHashMap();
+	private HashMap<UUID, Question> runningQuestions = Maps.newHashMap();
 	private SimpleIdGenerator IdGenerator = new SimpleIdGenerator();
-	private HashMap<UUID, Question> runningProcesses = Maps.newHashMap();
 
 	// /search?q=What+is+the+capital+of+Germany+%3F
 	@RequestMapping("/search")
@@ -33,11 +39,12 @@ public class WebController {
 		q.languageToQuestion.put("en", question);
 		q.UUID = IdGenerator.generateId();
 
-		// put it to queue to fetch while long lasting processing
-		runningProcesses.put(q.UUID, q);
-
 		// start the search process
-		startSearch(q);
+		Future<Question> report = asyncSearchExecutor.search(q);
+
+		// put it to queue to fetch while long lasting processing
+		runningProcesses.put(q.UUID, report);
+		runningQuestions.put(q.UUID, q);
 
 		// return the UUID
 		return q.UUID;
@@ -46,26 +53,13 @@ public class WebController {
 	@RequestMapping("/status")
 	public String status(@RequestParam(value = "UUID") UUID UUID) {
 		if (runningProcesses.containsKey(UUID)) {
-			Question q = runningProcesses.get(UUID);
-			if (q.finished) {
+			Future<Question> q = runningProcesses.get(UUID);
+			if (q.isDone()) {
 				runningProcesses.remove(UUID);
 			}
-			return q.getJSONStatus();
-		} else {
-			return "{Error: \"No such search id.\"}";
+			return runningQuestions.get(UUID).getJSONStatus();
 		}
+		return "{Error: \"No such search id.\"}";
 	}
 
-	private void startSearch(Question q) {
-		log.info("Run pipeline on " + q.languageToQuestion.get("en"));
-		List<Answer> answers = pipeline.getAnswersToQuestion(q);
-
-		// FIXME improve ranking
-		// bucket-based ranking
-		BucketRanker bucket_ranker = new BucketRanker();
-		log.info("Bucket-based ranking");
-		List<Answer> rankedAnswer = bucket_ranker.rank(answers, q);
-		log.info(Joiner.on("\n\t").join(rankedAnswer));
-		q.finalAnswer = rankedAnswer;
-	}
 }
