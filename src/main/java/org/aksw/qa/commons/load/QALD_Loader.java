66cd09d59cb6b7f4d19421917df2534d71336685
TortugaAttack <mai12cpg@studserv.uni-leipzig.de>
2015-11-26 17:37:06 +0100

    TODOs finished

--- src/main/java/org/aksw/qa/commons/load/QALD_Loader.java
+++ src/main/java/org/aksw/qa/commons/load/QALD_Loader.java
@@ -1,5 +1,7 @@
 package org.aksw.qa.commons.load;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -24,7 +26,47 @@ import org.xml.sax.SAXException;
  *
  */
 public class QALD_Loader {
-//TODO somehow read all folders in qa-datasets and provide each benchmark in an enumeration
+
+
+	private static List<File> iterateOverFiles(File parent) {
+		List<File> ret = new ArrayList<File>();
+		for (File f : parent.listFiles()) {
+			if (f.isDirectory()) {
+				ret.addAll(iterateOverFiles(f));
+			} else {
+
+				ret.add(f);
+			}
+		}
+		return ret;
+	}
+
+	public static List<Question> loadAll(String parentFolder)
+			throws IOException {
+		List<Question> ret = new ArrayList<Question>();
+
+		File parent = new File(parentFolder);
+		if (!parent.isDirectory()) {
+			throw new IOException(parent.getName() + " is not a directory");
+		}
+
+		for (File f : iterateOverFiles(parent)) {
+			try {
+				InputStream fis = new FileInputStream(f);
+				System.out.println(f.getName());
+				List<Question> ql = load(fis);
+				for (Question q : ql) {
+					System.out.println(q.languageToQuestion);
+				}
+				ret.addAll(ql);
+				fis.close();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+		return ret;
+	}
+
 	public static List<Question> load(InputStream file) {
 
 		List<Question> questions = new ArrayList<Question>();
@@ -45,26 +87,36 @@ public class QALD_Loader {
 
 				question.id = Integer.valueOf(questionNode.getAttribute("id"));
 				question.answerType = questionNode.getAttribute("answertype");
-				question.aggregation = Boolean.valueOf(questionNode.getAttribute("aggregation"));
-				question.onlydbo = Boolean.valueOf(questionNode.getAttribute("onlydbo"));
-				question.hybrid = Boolean.valueOf(questionNode.getAttribute("hybrid"));
+				question.aggregation = Boolean.valueOf(questionNode
+						.getAttribute("aggregation"));
+				question.onlydbo = Boolean.valueOf(questionNode
+						.getAttribute("onlydbo"));
+				question.hybrid = Boolean.valueOf(questionNode
+						.getAttribute("hybrid"));
 
 				// Read question
 				NodeList nlrs = questionNode.getElementsByTagName("string");
 				for (int j = 0; j < nlrs.getLength(); j++) {
 					String lang = ((Element) nlrs.item(j)).getAttribute("lang");
-					question.languageToQuestion.put(lang, ((Element) nlrs.item(j)).getTextContent().trim());
+					question.languageToQuestion.put(lang,
+							((Element) nlrs.item(j)).getTextContent().trim());
 				}
 
 				// read keywords
-				NodeList keywords = questionNode.getElementsByTagName("keywords");
+				NodeList keywords = questionNode
+						.getElementsByTagName("keywords");
 				for (int j = 0; j < keywords.getLength(); j++) {
-					String lang = ((Element) keywords.item(j)).getAttribute("lang");
-					question.languageToKeywords.put(lang, Arrays.asList(((Element) keywords.item(j)).getTextContent().trim().split(", ")));
+					String lang = ((Element) keywords.item(j))
+							.getAttribute("lang");
+					question.languageToKeywords.put(
+							lang,
+							Arrays.asList(((Element) keywords.item(j))
+									.getTextContent().trim().split(", ")));
 				}
 
 				// Read pseudoSPARQL query
-				Element element = (Element) questionNode.getElementsByTagName("pseudoquery").item(0);
+				Element element = (Element) questionNode.getElementsByTagName(
+						"pseudoquery").item(0);
 				if (element != null) {
 					NodeList childNodes = element.getChildNodes();
 					Node item = childNodes.item(0);
@@ -72,7 +124,8 @@ public class QALD_Loader {
 				}
 
 				// Read SPARQL query
-				element = (Element) questionNode.getElementsByTagName("query").item(0);
+				element = (Element) questionNode.getElementsByTagName("query")
+						.item(0);
 				if (element != null) {
 					NodeList childNodes = element.getChildNodes();
 					Node item = childNodes.item(0);
@@ -80,13 +133,15 @@ public class QALD_Loader {
 				}
 				// check if OUT OF SCOPE marked
 				if (question.pseudoSparqlQuery != null) {
-					question.outOfScope = question.pseudoSparqlQuery.toUpperCase().contains("OUT OF SCOPE");
+					question.outOfScope = question.pseudoSparqlQuery
+							.toUpperCase().contains("OUT OF SCOPE");
 				}
 				// Read answers
 				NodeList answers = questionNode.getElementsByTagName("answer");
 				HashSet<String> set = Sets.newHashSet();
 				for (int j = 0; j < answers.getLength(); j++) {
-					String answer = ((Element) answers.item(j)).getTextContent();
+					String answer = ((Element) answers.item(j))
+							.getTextContent();
 					set.add(answer.trim());
 				}
 				question.goldenAnswers = set;
--- src/test/java/org/aksw/qa/commons/measure/SPARQLEvaluationTest.java
+++ src/test/java/org/aksw/qa/commons/measure/SPARQLEvaluationTest.java
@@ -1,7 +1,12 @@
 package org.aksw.qa.commons.measure;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,20 +15,54 @@ public class SPARQLEvaluationTest {
 	String endpoint = "http://dbpedia.org/sparql";
 	Logger logger = LoggerFactory.getLogger(SPARQLEvaluationTest.class);
 
-	// TODO add test whether endpoint is available
+	@Test
+	public void testEndpointAvailibility() {
+		// Query should be validate for every Endpoint
+		String queryString = "SELECT * WHERE {?s ?p ?o} LIMIT 1";
+		// Validates Query String itself
+		Query query = QueryFactory.create(queryString);
+		QueryEngineHTTP qe = null;
+		try {
+			qe = new QueryEngineHTTP(endpoint, query);
+			ResultSet res = qe.execSelect();
+			// Is ResultSet == null Endpoint is with a high probability not
+			// available
+			assertNotNull(res);
+		} catch (Exception e) {
+			logger.debug("Endpoint is not available due to: ", e);
+			assertNotNull(null);
+		} finally {
+			// close QueryEngine
+			if (qe != null) {
+				qe.close();
+			}
+		}
+	}
+
 	@Test
 	public void testTooGenericQuery() {
-		String sparqlQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> " + "PREFIX res: <http://dbpedia.org/resource/> " + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-		        + "SELECT DISTINCT ?uri WHERE {	" + "?uri rdf:type dbo:Film ." + "?uri dbo:starring res:Julia_Roberts .}";
-		String targetSPARQLQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> " + "PREFIX res: <http://dbpedia.org/resource/> " + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-		        + "SELECT DISTINCT ?uri WHERE {	" + "?uri rdf:type dbo:Film ." + "?uri dbo:starring res:Julia_Roberts ." + "?uri dbo:director res:Garry_Marshall .}";
-		double precision = SPARQLEvaluation.precision(sparqlQuery, targetSPARQLQuery, endpoint);
-		double recall = SPARQLEvaluation.recall(sparqlQuery, targetSPARQLQuery, endpoint);
-		double fMeasure = SPARQLEvaluation.fMeasure(sparqlQuery, targetSPARQLQuery, endpoint);
+		String sparqlQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> "
+				+ "PREFIX res: <http://dbpedia.org/resource/> "
+				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+				+ "SELECT DISTINCT ?uri WHERE {	" + "?uri rdf:type dbo:Film ."
+				+ "?uri dbo:starring res:Julia_Roberts .}";
+		String targetSPARQLQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> "
+				+ "PREFIX res: <http://dbpedia.org/resource/> "
+				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+				+ "SELECT DISTINCT ?uri WHERE {	"
+				+ "?uri rdf:type dbo:Film ."
+				+ "?uri dbo:starring res:Julia_Roberts ."
+				+ "?uri dbo:director res:Garry_Marshall .}";
+		double precision = SPARQLEvaluation.precision(sparqlQuery,
+				targetSPARQLQuery, endpoint);
+		double recall = SPARQLEvaluation.recall(sparqlQuery, targetSPARQLQuery,
+				endpoint);
+		double fMeasure = SPARQLEvaluation.fMeasure(sparqlQuery,
+				targetSPARQLQuery, endpoint);
 		assertEquals(0.0571, precision, 0.001);
 		assertEquals(1.0, recall, 0.0);
 		assertEquals(0.108, fMeasure, 0.001);
-		
+
 		logger.debug(Thread.currentThread().getStackTrace()[1].getMethodName());
 		logger.debug("P=" + precision);
 		logger.debug("R=" + recall);
@@ -34,70 +73,82 @@ public class SPARQLEvaluationTest {
 	@Test
 	public void testTooSpecificQuery() {
 		// SELECT COUNT(?x)...
-		String sparqlQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> " + "PREFIX res: <http://dbpedia.org/resource/> " + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-		        + "SELECT COUNT(DISTINCT ?uri) WHERE {	" + "?uri rdf:type dbo:Film ." + "?uri dbo:starring res:Julia_Roberts .}";
-		String targetSPARQLQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> " + "PREFIX res: <http://dbpedia.org/resource/> " + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
-		        + "SELECT COUNT(DISTINCT ?uri) WHERE {	" + "?uri rdf:type dbo:Film ." + "?uri dbo:starring res:Julia_Roberts ." + "?uri dbo:director res:Garry_Marshall .}";
-		double precision = SPARQLEvaluation.precision(sparqlQuery, targetSPARQLQuery, endpoint);
-		double recall = SPARQLEvaluation.recall(sparqlQuery, targetSPARQLQuery, endpoint);
-		double fMeasure = SPARQLEvaluation.fMeasure(sparqlQuery, targetSPARQLQuery, endpoint);
-		
+		String sparqlQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> "
+				+ "PREFIX res: <http://dbpedia.org/resource/> "
+				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+				+ "SELECT COUNT(DISTINCT ?uri) WHERE {	"
+				+ "?uri rdf:type dbo:Film ."
+				+ "?uri dbo:starring res:Julia_Roberts .}";
+		String targetSPARQLQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> "
+				+ "PREFIX res: <http://dbpedia.org/resource/> "
+				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+				+ "SELECT COUNT(DISTINCT ?uri) WHERE {	"
+				+ "?uri rdf:type dbo:Film ."
+				+ "?uri dbo:starring res:Julia_Roberts ."
+				+ "?uri dbo:director res:Garry_Marshall .}";
+		double precision = SPARQLEvaluation.precision(sparqlQuery,
+				targetSPARQLQuery, endpoint);
+		double recall = SPARQLEvaluation.recall(sparqlQuery, targetSPARQLQuery,
+				endpoint);
+		double fMeasure = SPARQLEvaluation.fMeasure(sparqlQuery,
+				targetSPARQLQuery, endpoint);
+
 		logger.debug(Thread.currentThread().getStackTrace()[1].getMethodName());
 		logger.debug("P=" + precision);
 		logger.debug("R=" + recall);
-		logger.debug("F=" + fMeasure); 
-		
-		//TODO also transform to unit test
-		
-		// String sparqlQuery =
-									   // "PREFIX dbo: <http://dbpedia.org/ontology/> "
-									   // +
-		// "PREFIX res: <http://dbpedia.org/resource/> " +
-		// "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-		// "SELECT DISTINCT ?uri WHERE {	" +
-		// "?uri rdf:type dbo:Film ." +
-		// "?uri dbo:starring res:Julia_Roberts .}";
-		// String targetSPARQLQuery =
-		// "PREFIX dbo: <http://dbpedia.org/ontology/> " +
-		// "PREFIX res: <http://dbpedia.org/resource/> " +
-		// "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-		// "SELECT DISTINCT ?uri WHERE {	" +
-		// "?uri rdf:type dbo:Film ." +
-		// "?uri dbo:starring res:Julia_Roberts ." +
-		// "?uri dbo:director res:Garry_Marshall .}";
-		// SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpedia();
-		// double precision = SPARQLEvaluation.precision(sparqlQuery,
-		// targetSPARQLQuery, endpoint);
-		// double recall = SPARQLEvaluation.recall(sparqlQuery,
-		// targetSPARQLQuery, endpoint);
-		// double fMeasure = SPARQLEvaluation.fMeasure(sparqlQuery,
-		// targetSPARQLQuery, endpoint);
-		// System.out.println("P=" + precision + "\nR=" + recall + "\nF=" +
-		// fMeasure);
-		//
-		// //SELECT COUNT(?x)...
-		// sparqlQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> " +
-		// "PREFIX res: <http://dbpedia.org/resource/> " +
-		// "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-		// "SELECT COUNT(DISTINCT ?uri) WHERE {	" +
-		// "?uri rdf:type dbo:Film ." +
-		// "?uri dbo:starring res:Julia_Roberts .}";
-		// targetSPARQLQuery =
-		// "PREFIX dbo: <http://dbpedia.org/ontology/> " +
-		// "PREFIX res: <http://dbpedia.org/resource/> " +
-		// "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-		// "SELECT COUNT(DISTINCT ?uri) WHERE {	" +
-		// "?uri rdf:type dbo:Film ." +
-		// "?uri dbo:starring res:Julia_Roberts ." +
-		// "?uri dbo:director res:Garry_Marshall .}";
-		// precision = SPARQLEvaluation.precision(sparqlQuery,
-		// targetSPARQLQuery, endpoint);
-		// recall = SPARQLEvaluation.recall(sparqlQuery, targetSPARQLQuery,
-		// endpoint);
-		// fMeasure = SPARQLEvaluation.fMeasure(sparqlQuery, targetSPARQLQuery,
-		// endpoint);
-		// System.out.println("P=" + precision + "\nR=" + recall + "\nF=" +
-		// fMeasure);
-
+		logger.debug("F=" + fMeasure);
 	}
+
+	// TODO also transform to unit test
+//	@Test
+//	public void testWhatEver() {
+//		String sparqlQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> "
+//				+ "PREFIX res: <http://dbpedia.org/resource/> "
+//				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+//				+ "SELECT DISTINCT ?uri WHERE {	" + "?uri rdf:type dbo:Film ."
+//				+ "?uri dbo:starring res:Julia_Roberts .}";
+//		String targetSPARQLQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> "
+//				+ "PREFIX res: <http://dbpedia.org/resource/> "
+//				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+//				+ "SELECT DISTINCT ?uri WHERE {	"
+//				+ "?uri rdf:type dbo:Film ."
+//				+ "?uri dbo:starring res:Julia_Roberts ."
+//				+ "?uri dbo:director res:Garry_Marshall .}";
+//		double precision = SPARQLEvaluation.precision(sparqlQuery,
+//				targetSPARQLQuery, endpoint);
+//		double recall = SPARQLEvaluation.recall(sparqlQuery, targetSPARQLQuery,
+//				endpoint);
+//		double fMeasure = SPARQLEvaluation.fMeasure(sparqlQuery,
+//				targetSPARQLQuery, endpoint);
+//		logger.debug(Thread.currentThread().getStackTrace()[1].getMethodName());
+//		logger.debug("P=" + precision);
+//		logger.debug("R=" + recall);
+//		logger.debug("F=" + fMeasure);
+//
+//		// SELECT COUNT(?x)...
+//		sparqlQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> "
+//				+ "PREFIX res: <http://dbpedia.org/resource/> "
+//				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+//				+ "SELECT COUNT(DISTINCT ?uri) WHERE {	"
+//				+ "?uri rdf:type dbo:Film ."
+//				+ "?uri dbo:starring res:Julia_Roberts .}";
+//		targetSPARQLQuery = "PREFIX dbo: <http://dbpedia.org/ontology/> "
+//				+ "PREFIX res: <http://dbpedia.org/resource/> "
+//				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+//				+ "SELECT COUNT(DISTINCT ?uri) WHERE {	"
+//				+ "?uri rdf:type dbo:Film ."
+//				+ "?uri dbo:starring res:Julia_Roberts ."
+//				+ "?uri dbo:director res:Garry_Marshall .}";
+//		precision = SPARQLEvaluation.precision(sparqlQuery, targetSPARQLQuery,
+//				endpoint);
+//		recall = SPARQLEvaluation.recall(sparqlQuery, targetSPARQLQuery,
+//				endpoint);
+//		fMeasure = SPARQLEvaluation.fMeasure(sparqlQuery, targetSPARQLQuery,
+//				endpoint);
+//		logger.debug(Thread.currentThread().getStackTrace()[1].getMethodName());
+//		logger.debug("P=" + precision);
+//		logger.debug("R=" + recall);
+//		logger.debug("F=" + fMeasure);
+//
+//	}
 }
