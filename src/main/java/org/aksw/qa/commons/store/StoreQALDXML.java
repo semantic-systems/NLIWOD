db1dfe94da8a1678e4dd7ea778f710bbfe1ee056
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2015-11-26 10:22:29 +0100

    enhanced store routine comment

--- src/main/java/org/aksw/qa/commons/store/StoreQALDXML.java
+++ src/main/java/org/aksw/qa/commons/store/StoreQALDXML.java
@@ -1,6 +1,7 @@
 package org.aksw.qa.commons.store;
 
 //TODO fill so it can be submitted to the QALD website
+//so the eval tool works http://greententacle.techfak.uni-bielefeld.de/~cunger/qald/index.php?x=evaltool&q=5 
 public class StoreQALDXML {
 
 }
