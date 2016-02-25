e52c2f7734937569bea85b3d890e8b6ffe5a0a02
Jonathan Huthmann <Jonathan@TPJonathan>
2016-02-25 15:41:39 +0100

    ready to read QALD6 datasets

---
+++ src/main/java/org/aksw/autosparql/commons/qald/QALD6_Pipeline.java
@@ -0,0 +1,101 @@
+package org.aksw.autosparql.commons.qald;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Set;
+
+import org.aksw.hawk.controller.EvalObj;
+import org.aksw.hawk.controller.Pipeline;
+import org.aksw.hawk.datastructures.Answer;
+import org.aksw.hawk.datastructures.Question;
+import org.aksw.hawk.experiment.Measures;
+import org.aksw.hawk.experiment.TrainingPipeline;
+import org.aksw.hawk.querybuilding.SPARQLQuery;
+import org.aksw.hawk.ranking.FeatureBasedRanker;
+import org.aksw.hawk.ranking.OptimalRanker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Sets;
+
+public class QALD6_Pipeline {
+    static Logger log = LoggerFactory.getLogger(TrainingPipeline.class);
+
+    public QALD6_Pipeline() {
+
+	log.info("Configuring controller");
+	Pipeline pipeline = new Pipeline();
+
+	log.info("Loading dataset");
+	URL url = ClassLoader.getSystemClassLoader().getResource(
+		"QALD-6/qald-6-train-hybrid.json");
+	List<Question> questions = null;
+	try {
+	    questions = QALD_Loader.loadJSON(url.openStream());
+
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    System.exit(0);
+	}
+
+	double average = 0;
+	double count = 0;
+	double countNULLAnswer = 0;
+	for (Question q : questions) {
+	    if ((q.answerType.equals("resource") & q.onlydbo & !q.aggregation)
+		    || q.loadedAsASKQuery) {
+		log.info("Run pipeline on " + q.languageToQuestion.get("en"));
+		List<Answer> answers = pipeline.getAnswersToQuestion(q);
+
+		if (answers.isEmpty()) {
+		    log.warn("Question#" + q.id + " returned no answers! (Q: "
+			    + q.languageToQuestion.get("en") + ")");
+		    ++countNULLAnswer;
+		    continue;
+		}
+		++count;
+
+		// ##############~~RANKING~~##############
+		log.info("Run ranking");
+		int maximumPositionToMeasure = 10;
+		OptimalRanker optimal_ranker = new OptimalRanker();
+		FeatureBasedRanker feature_ranker = new FeatureBasedRanker();
+
+		// optimal ranking
+		log.info("Optimal ranking");
+		List<Answer> rankedAnswer = optimal_ranker.rank(answers, q);
+		List<EvalObj> eval = Measures.measure(rankedAnswer, q,
+			maximumPositionToMeasure);
+		log.debug(Joiner.on("\n\t").join(eval));
+
+		Set<SPARQLQuery> queries = Sets.newHashSet();
+		double fmax = 0;
+		for (EvalObj e : eval) {
+		    if (e.getFmax() == fmax) {
+			queries.add(e.getAnswer().query);
+		    } else if (e.getFmax() > fmax) {
+			queries.clear();
+			queries.add(e.getAnswer().query);
+			fmax = e.getFmax();
+		    }
+		}
+		log.info("Max F-measure: " + fmax);
+		average += fmax;
+		log.info("Feature-based ranking begins training.");
+		feature_ranker.learn(q, queries);
+	    }
+	}
+	log.info("Number of questions with answer: " + count
+		+ ", number of questions without answer: " + countNULLAnswer);
+	log.info("Average F-measure: " + (average / count));
+
+    }
+
+    public static void main(String args[]) {
+	new QALD6_Pipeline();
+
+    }
+
+}
--- src/main/java/org/aksw/hawk/datastructures/Question.java
+++ src/main/java/org/aksw/hawk/datastructures/Question.java
@@ -20,69 +20,103 @@ import org.json.simple.JSONObject;
 //TODO move to qa-commons
 public class Question implements Serializable {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 9130793012431486456L;
+    private static final long serialVersionUID = 9130793012431486456L;
 
-	public Integer id;
-	public int cardinality;
-	public Boolean onlydbo;
-	public Boolean outOfScope;
-	public Boolean aggregation;
-	public Boolean hybrid;
-	public Boolean isClassifiedAsASKQuery; // what the program classifies
-	public Boolean loadedAsASKQuery; // what we load from the data file
-	public String answerType;
-	public String sparqlQuery;
-	public String pseudoSparqlQuery;
-	public MutableTree tree;
-	public Map<String, String> languageToQuestion = new LinkedHashMap<String, String>();
-	public Map<String, List<String>> languageToKeywords = new LinkedHashMap<String, List<String>>();
-	public Map<String, List<Entity>> languageToNamedEntites = new LinkedHashMap<String, List<Entity>>();
-	public Map<String, List<Entity>> languageToNounPhrases = new LinkedHashMap<String, List<Entity>>();
-	public Map<String, List<GoldEntity>> goldEntites = new HashMap<String, List<GoldEntity>>();
-	public Map<String, Set<String>> goldenAnswers = new HashMap<String, Set<String>>();
+    public Integer id;
+    public int cardinality;
+    public Boolean onlydbo;
+    public Boolean outOfScope;
+    public Boolean aggregation;
+    public Boolean hybrid;
+    public Boolean isClassifiedAsASKQuery; // what the program classifies
+    public Boolean loadedAsASKQuery; // what we load from the data file
+    public String answerType;
+    public String sparqlQuery;
+    public String pseudoSparqlQuery;
+    public MutableTree tree;
+    public Map<String, String> languageToQuestion = new LinkedHashMap<String, String>();
+    public Map<String, List<String>> languageToKeywords = new LinkedHashMap<String, List<String>>();
+    public Map<String, List<Entity>> languageToNamedEntites = new LinkedHashMap<String, List<Entity>>();
+    public Map<String, List<Entity>> languageToNounPhrases = new LinkedHashMap<String, List<Entity>>();
+    public Map<String, List<GoldEntity>> goldEntites = new HashMap<String, List<GoldEntity>>();
+    public Map<String, Set<String>> goldenAnswers = new HashMap<String, Set<String>>();
 
-	//for proper handling in webservice
-	public UUID UUID;
-	public boolean finished;
-	public List<Answer> finalAnswer;
-	public JSONObject tree_full;
-	public JSONObject tree_pruned;
-	public JSONObject tree_final;
-	public JSONArray pruning_messages=new JSONArray();
+    // for proper handling in webservice
+    public UUID UUID;
+    public boolean finished;
+    public List<Answer> finalAnswer;
+    public JSONObject tree_full;
+    public JSONObject tree_pruned;
+    public JSONObject tree_final;
+    public JSONArray pruning_messages = new JSONArray();
 
+    public Question() {
 
+	goldEntites.put("en", new ArrayList<GoldEntity>());
+	goldEntites.put("de", new ArrayList<GoldEntity>());
+    }
 
-	public Question() {
+    @Override
+    public String toString() {
 
-		goldEntites.put("en", new ArrayList<GoldEntity>());
-		goldEntites.put("de", new ArrayList<GoldEntity>());
+	String output = String.format(
+		"id: %s answerType: %s aggregation: %s onlydbo: %s\n", id,
+		answerType, aggregation, onlydbo);
+	for (Map.Entry<String, String> entry : languageToQuestion.entrySet()) {
+	    output += "\t" + entry.getKey() + "\tQuestion: " + entry.getValue()
+		    + "\n";
+	    output += "\t\tKeywords: "
+		    + StringUtils.join(languageToKeywords.get(entry.getKey()),
+			    ", ") + "\n";
+	    output += "\t\tGold-Entities: "
+		    + StringUtils.join(goldEntites, ", ") + "\n";
+	    if (languageToNamedEntites.containsKey(entry.getKey()))
+		output += "\t\tEntities: "
+			+ StringUtils.join(
+				languageToNamedEntites.get(entry.getKey()),
+				", ") + "\n";
+	    if (languageToNounPhrases.containsKey(entry.getKey()))
+		output += "\t\tNouns: "
+			+ StringUtils
+				.join(languageToNounPhrases.get(entry.getKey()),
+					", ") + "\n";
 	}
+	output += "PseudoSPARQL: " + pseudoSparqlQuery + "\n";
+	output += "SPARQL: " + sparqlQuery + "\n";
+	output += "Answers: " + StringUtils.join(goldenAnswers, ", ") + "\n";
 
-	@Override
-	public String toString() {
+	return output;
+    }
 
-		String output = String.format("id: %s answerType: %s aggregation: %s onlydbo: %s\n", id, answerType, aggregation, onlydbo);
-		for (Map.Entry<String, String> entry : languageToQuestion.entrySet()) {
-			output += "\t" + entry.getKey() + "\tQuestion: " + entry.getValue() + "\n";
-			output += "\t\tKeywords: " + StringUtils.join(languageToKeywords.get(entry.getKey()), ", ") + "\n";
-			output += "\t\tGold-Entities: " + StringUtils.join(goldEntites, ", ") + "\n";
-			if (languageToNamedEntites.containsKey(entry.getKey()))
-				output += "\t\tEntities: " + StringUtils.join(languageToNamedEntites.get(entry.getKey()), ", ") + "\n";
-			if (languageToNounPhrases.containsKey(entry.getKey()))
-				output += "\t\tNouns: " + StringUtils.join(languageToNounPhrases.get(entry.getKey()), ", ") + "\n";
-		}
-		output += "SPARQL: " + pseudoSparqlQuery;
-		output += "\tAnswers: " + StringUtils.join(goldenAnswers, ", ") + "\n";
+    public String getJSONStatus() {
+	JSONObject sb = JSONStatusBuilder.status(this);
+	return sb.toJSONString();
 
-		return output;
+    }
+
+    public void setValue(String valDescriptor, String val) {
+	valDescriptor = valDescriptor.toLowerCase();
+	switch (valDescriptor) {
+	case "id":
+	    this.id = Integer.parseInt(val);
+	    break;
+	case "answertype":
+	    this.answerType = val;
+	    break;
+	case "aggregation":
+	    this.aggregation = Boolean.parseBoolean(val);
+	    break;
+	case "onlydbo":
+	    this.onlydbo = Boolean.parseBoolean(val);
+	    break;
+	case "hybrid":
+	    this.hybrid = Boolean.parseBoolean(val);
+	    break;
+	default:
+	    ;
 	}
-
-	public String getJSONStatus() {
-		JSONObject sb = JSONStatusBuilder.status(this);
-		return sb.toJSONString();
-
-	}
+    }
 }
