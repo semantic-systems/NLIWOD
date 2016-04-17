2f9e93d6c105e0f4fe8066306648ea140bb7afc7
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2016-04-17 20:01:39 +0200

    added a test that fails on purpose

--- src/test/java/org/aksw/qa/commons/load/LoadTest.java
+++ src/test/java/org/aksw/qa/commons/load/LoadTest.java
@@ -1,19 +1,40 @@
 package org.aksw.qa.commons.load;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
 import org.aksw.qa.commons.datastructure.IQuestion;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoadTest {
+	Logger log = LoggerFactory.getLogger(LoadTest.class);
 
 	@Test
-	// TODO use small snippets of test files under src/test/resources instead of
-	// qa-datasets so we can get rid of the dependency
+	public void testAllDatasetsTowardsLoadibility() {
+		for (Dataset d : Dataset.values()) {
+			try {
+				List<IQuestion> questions = QALD_Loader.load(d);
+				log.info("Dataset succesfully loaded:" + d.name());
+				for (IQuestion q : questions) {
+					Assert.assertTrue(q.getId() > 0);
+					Assert.assertNotNull(q.getAnswerType());
+					Assert.assertTrue(q.getPseudoSparqlQuery() != null || q.getSparqlQuery() != null);
+					Assert.assertNotNull(q.getLanguageToQuestion());
+					Assert.assertFalse(q.getLanguageToQuestion().values().isEmpty());
+					Assert.assertNotNull(q.getLanguageToKeywords());
+					Assert.assertTrue(q.getGoldenAnswers() != null && q.getAnswerType().matches("resource||boolean||number||date||string"));
+				}
+			} catch (Exception e) {
+				log.error("Dataset couldn't be loaded:" + d.name());
+			}
+		}
+	}
+
+	@Test
 	public void loadQALD5Test() {
 		List<IQuestion> load = QALD_Loader.load(Dataset.QALD5_Test);
 		Assert.assertTrue(load.size() == 59);
@@ -24,41 +45,32 @@ public class LoadTest {
 			Assert.assertNotNull(q.getLanguageToQuestion());
 			Assert.assertFalse(q.getLanguageToQuestion().values().isEmpty());
 			Assert.assertNotNull(q.getLanguageToKeywords());
-			System.out.println(q);
 			Assert.assertTrue(q.getGoldenAnswers() != null && q.getAnswerType().matches("resource||boolean||number||date||string"));
 		}
 	}
-	
+
 	@Test
-	// TODO use small snippets of test files under src/test/resources instead of
-	// qa-datasets so we can get rid of the dependency
-	//TODO update test
-	public void loadQALD6Test() throws IOException {
-		//URL url= ClassLoader.getSystemClassLoader().getResource("QALD-6/qald-6-train-multilingual.json");
+	public void loadQALD6Test_Multilingual() throws IOException {
 		List<IQuestion> load = QALD_Loader.load(Dataset.QALD6_Train_Multilingual);
-		Assert.assertTrue(load.size() == 350);
-		List<Integer>incompletes=Arrays.asList(100, 118, 136, 137, 147, 152, 94, 95, 96, 97, 98, 99, 249, 250, 312, 340, 342);
+		List<Integer> incompletes = Arrays.asList(100, 118, 136, 137, 147, 152, 94, 95, 96, 97, 98, 99, 249, 250, 312, 340, 342);
+		log.debug("Number of Loaded Questions:" + load.size());
+		Assert.assertTrue(load.size() == 350 - incompletes.size());
 		for (IQuestion q : load) {
-			System.out.print("Testing: "+q.getId()+"\t");
 			Assert.assertTrue(q.getId() > 0);
 			Assert.assertNotNull(q.getAnswerType());
 			Assert.assertTrue(q.getGoldenAnswers() != null && q.getAnswerType().matches("resource||boolean||number||date||string"));
 			Assert.assertNotNull(q.getLanguageToQuestion());
 			Assert.assertFalse(q.getLanguageToQuestion().values().isEmpty());
 			Assert.assertNotNull(q.getLanguageToKeywords());
-			
-			//skipping Answer on known incompletes:
-			if (!incompletes.contains(q.getId()))
-			{
-				
-			
+
+			// skipping Answer on known incompletes:
+			if (!incompletes.contains(q.getId())) {
 				Assert.assertTrue(q.getPseudoSparqlQuery() != null || q.getSparqlQuery() != null);
-				System.out.println(q.getLanguageToQuestion().get("en")+"\t"+"Answer:"+"\t"+q.getSparqlQuery());
-				}
-			else
-			{
-				System.out.println(q.getLanguageToQuestion().get("en")+"\t"+"No Answer, known incomplete question.");
+				log.debug(q.getLanguageToQuestion().get("en") + "\t" + "Answer:" + "\t" + q.getSparqlQuery());
+			} else {
+				log.debug(q.getLanguageToQuestion().get("en") + "\t" + "No Answer, known incomplete question.");
 			}
 		}
 	}
+
 }
