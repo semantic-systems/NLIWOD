b0584e6404e1651af1927834b815bd53f034aaeb
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2016-07-05 18:00:51 +0200

    Investigated usefullness of natural logic and constituent parsing. result: two possible new noun phrase recognizers for HAWK

---
+++ qa.hawk/src/main/java/org/aksw/hawk/experiment/ConstituentParsingStanford.java
@@ -0,0 +1,72 @@
+package org.aksw.hawk.experiment;
+
+import java.util.List;
+
+import org.aksw.hawk.datastructures.HAWKQuestion;
+import org.aksw.hawk.datastructures.HAWKQuestionFactory;
+import org.aksw.qa.commons.datastructure.Question;
+import org.aksw.qa.commons.load.Dataset;
+import org.aksw.qa.commons.load.QALD_Loader;
+
+import edu.stanford.nlp.simple.Document;
+import edu.stanford.nlp.simple.Sentence;
+import edu.stanford.nlp.trees.Constituent;
+import edu.stanford.nlp.trees.LabeledScoredConstituent;
+import edu.stanford.nlp.trees.Tree;
+
+public class ConstituentParsingStanford {
+	// TODO after transforming to classes, extent NounphraseIdentification class
+	// and benchmark their behaviour in hybrid scenario
+	public static void main(String[] args) {
+
+		List<HAWKQuestion> questions = null;
+
+		questions = HAWKQuestionFactory.createInstances(QALD_Loader.load(Dataset.QALD6_Train_Hybrid));
+		// TODO transform both loops to nounphrase extractor classes
+
+		// TODO it could be helpful to also run NER and excluded recognized
+		// entities
+
+		// TODO remove overlapping NP
+		for (Question q : questions) {
+			String text = q.getLanguageToQuestion().get("en");
+			System.out.println("\n" + text);
+			Document doc = new Document(text);
+			for (Sentence sent : doc.sentences()) {
+				for (Tree tree : sent.parse().subTreeList()) {
+					if (tree.isPhrasal()) {
+						if (tree.label().value().equals("NP")) {
+							if (tree.children().length > 1) {
+								// check that subtree does not contain a PP or a
+								// VP
+								boolean containsPPorVP = false;
+								for (Tree subtree : tree.subTreeList()) {
+									if (subtree.isPhrasal() && subtree.label().value().matches("PP||VP")) {
+										containsPPorVP = true;
+										break;
+									}
+								}
+								if (!containsPPorVP) {
+									System.out.println(tree.toString());
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		// TODO this can definitely used as noun phrase extracted
+		for (Question q : questions) {
+			String text = q.getLanguageToQuestion().get("en");
+			System.out.println("\n" + text);
+			Document doc = new Document(text);
+			for (Sentence sent : doc.sentences()) {
+				for (String keyphrase : sent.algorithms().keyphrases()) {
+					System.out.println(keyphrase);
+				}
+			}
+		}
+
+	}
+}
---
+++ qa.hawk/src/main/java/org/aksw/hawk/experiment/NatLogStanford.java
@@ -0,0 +1,50 @@
+package org.aksw.hawk.experiment;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+
+import org.aksw.hawk.datastructures.HAWKQuestion;
+import org.aksw.hawk.datastructures.HAWKQuestionFactory;
+import org.aksw.qa.commons.datastructure.Question;
+import org.aksw.qa.commons.load.Dataset;
+import org.aksw.qa.commons.load.QALD_Loader;
+
+import edu.stanford.nlp.ie.util.RelationTriple;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.naturalli.NaturalLogicAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+//comment by Ricardo
+//this looks promising although I am not convinced that it is useful
+
+public class NatLogStanford {
+	// http://stackoverflow.com/questions/32419534/stanford-openie-example-code-would-not-run-properly
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize,ssplit,pos,lemma,depparse,natlog,openie");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+		List<HAWKQuestion> questions = null;
+
+		questions = HAWKQuestionFactory.createInstances(QALD_Loader.load(Dataset.QALD6_Train_Hybrid));
+
+		for (Question q : questions) {
+			String text = q.getLanguageToQuestion().get("en");
+			System.out.println("\n" + text);
+			Annotation doc = new Annotation(text);
+			pipeline.annotate(doc);
+
+			for (CoreMap sentence : doc.get(CoreAnnotations.SentencesAnnotation.class)) {
+				Collection<RelationTriple> triples = sentence.get(NaturalLogicAnnotations.RelationTriplesAnnotation.class);
+				for (RelationTriple triple : triples) {
+					System.out.println(triple.confidence + "\t" + triple.subjectLemmaGloss() + "\t" + triple.relationLemmaGloss() + "\t" + triple.objectLemmaGloss());
+				}
+			}
+		}
+
+	}
+
+}
