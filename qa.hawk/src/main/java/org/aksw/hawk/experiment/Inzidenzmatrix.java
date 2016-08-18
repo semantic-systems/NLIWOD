2a19ae96e92b34ecfc937dd2743c587f13d061e5
micha <michoffmann.potsdam@gmail.com>
2016-08-18 15:36:44 +0200

    cleaned up a bit.

--- qa.hawk/src/main/java/org/aksw/hawk/experiment/Inzidenzmatrix.java
+++ qa.hawk/src/main/java/org/aksw/hawk/experiment/Inzidenzmatrix.java
@@ -47,20 +47,20 @@ public class Inzidenzmatrix {
 
 			// tranform graph to incidence matrix M with M_ij = 1 if v_i is in e_j
 			// just iterate over all edges e_j = (v_j1, v_j2) 
-			List<SemanticGraphEdge> edges = graph.edgeListSorted();
+			SemanticGraphEdge[] edges = graph.edgeListSorted().stream().toArray(SemanticGraphEdge[]::new);
 			IndexedWord[] vertices = graph.vertexSet().stream().toArray(IndexedWord[]::new);
 			// create incidence Matrix
-			Integer[][] incidenceMatrix = new Integer[vertices.length][edges.size()];
+			Integer[][] incidenceMatrix = new Integer[vertices.length][edges.length];
 			for(int i=0; i < vertices.length; i++){
-				for(int j=0; j < edges.size(); j++){
+				for(int j=0; j < edges.length; j++){
 					incidenceMatrix[i][j] = 0;
 				}
 			}
 			// fill incidence matrix
 			for(int i=0; i < vertices.length; i++){
-				for(int j=0; j < edges.size(); j++){
+				for(int j=0; j < edges.length; j++){
 					IndexedWord vertex = vertices[i];
-					SemanticGraphEdge edge = edges.get(j);
+					SemanticGraphEdge edge = edges[j];
 					// Mij = 1 if v_i is in e_j, and our graph is undirected
 					if(vertex.equals(edge.getSource()) || vertex.equals(edge.getTarget())){
 						incidenceMatrix[i][j] = 1;
