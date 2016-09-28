1969b5230e3ee6d42d004656c909e47e74f62248
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2016-09-28 11:19:37 +0200

    Added qanary

--- qa.seqa/src/main/java/qa/seqa/owl/InputOutputCompatibility.java
+++ qa.seqa/src/main/java/qa/seqa/owl/InputOutputCompatibility.java
@@ -7,13 +7,20 @@ import java.net.URL;
 import java.util.Set;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.IRIDocumentSource;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyLoaderConfiguration;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.Node;
+import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.util.OWLOntologyMerger;
 import org.springframework.stereotype.Service;
+
+import uk.ac.manchester.cs.owl.owlapi.OWLEquivalentClassesAxiomImpl;
 
 import com.clarkparsia.pellet.owlapiv3.PelletReasoner;
 import com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory;
@@ -32,8 +39,10 @@ public class InputOutputCompatibility {
 	public static void main(String[] args) throws Exception {
 		// load the importing ontology
 		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+
 		OWLOntology ontologyNIF = null;
 		OWLOntology ontologyOA = null;
+		OWLOntology ontologyQANARY = null;
 
 		try {
 			URL nifURL = new URL("https://raw.githubusercontent.com/NLP2RDF/ontologies/master/nif-core/nif-core.ttl");
@@ -46,6 +55,13 @@ public class InputOutputCompatibility {
 			InputStream oais = OAURL.openStream();
 			ontologyOA = manager.loadOntologyFromOntologyDocument(oais);
 
+			manager.getIRIMappers().clear();
+			OWLOntologyLoaderConfiguration config = new OWLOntologyLoaderConfiguration();
+			//FIXME replace the IRI below with https://www.w3.org/ns/oa/index.owl as soon as bug is fixed within QANARY
+			IRI create = IRI.create("http://www.openannotation.org/spec/core/20130208/oa.owl");
+			config = config.addIgnoredImport(create);
+			IRI qanaryURL = IRI.create("https://raw.githubusercontent.com/WDAqua/QAOntology/master/qanary.ttl");
+			ontologyQANARY = manager.loadOntologyFromOntologyDocument(new IRIDocumentSource(qanaryURL), config);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (OWLOntologyCreationException e) {
@@ -58,14 +74,20 @@ public class InputOutputCompatibility {
 		IRI mergedOntologyIRI = IRI.create("http://aksw.org/SEQA");
 		OWLOntology merged = merger.createMergedOntology(manager, mergedOntologyIRI);
 
+		System.out.println("class tree");
 		PelletReasoner r = PelletReasonerFactory.getInstance().createReasoner(merged);
-		System.out.println("done.");
 		r.getKB().realize();
 		r.getKB().printClassTree();
 
+		System.out.println("subclasses of owl:Thing");
 		OWLClass thing = merged.getOWLOntologyManager().getOWLDataFactory().getOWLClass(IRI.create("owl:Thing"));
 		Set<OWLClass> classes = r.getSubClasses(thing, false).getFlattened();
 		System.out.println(classes);
+
+		System.out.println("Equivalent classes");
+		NodeSet<OWLClass> clss = r.getSubClasses(manager.getOWLDataFactory().getOWLThing(),false);
+		clss.forEach(System.out::println);
+
 	}
 
 }
