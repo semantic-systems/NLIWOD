c24736b0da377d77baa6921a99856456f0d3f058
TortugaAttack <felixconrads@googlemail.com>
2016-11-03 19:40:49 +0100

    ASystems now throws Exception if responding Statuscode is 4xx or greater

--- qa.systems/src/main/java/org/aksw/qa/systems/QAKIS.java
+++ qa.systems/src/main/java/org/aksw/qa/systems/QAKIS.java
@@ -23,68 +23,71 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class QAKIS extends ASystem {
-    Logger log = LoggerFactory.getLogger(QAKIS.class);
+	Logger log = LoggerFactory.getLogger(QAKIS.class);
 
-    public String name() {
-        return "qakis";
-    };
+	public String name() {
+		return "qakis";
+	};
 
-    public void search(IQuestion question) {
-        String questionString;
-        if (!question.getLanguageToQuestion().containsKey("en")) {
-            return;
-        }
-        questionString = question.getLanguageToQuestion().get("en");
-        log.debug(this.getClass().getSimpleName() + ": " + questionString);
-        try {
-            final HashSet<String> resultSet = new HashSet<String>();
-            String url = "http://qakis.org/qakis/index.xhtml";
+	public void search(IQuestion question) throws Exception {
+		String questionString;
+		if (!question.getLanguageToQuestion().containsKey("en")) {
+			return;
+		}
+		questionString = question.getLanguageToQuestion().get("en");
+		log.debug(this.getClass().getSimpleName() + ": " + questionString);
+		final HashSet<String> resultSet = new HashSet<String>();
+		String url = "http://qakis.org/qakis/index.xhtml";
 
-            HttpClient client = HttpClientBuilder.create().build();
-            HttpPost httppost = new HttpPost(url);
-            HttpResponse ping = client.execute(httppost);
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpPost httppost = new HttpPost(url);
+		HttpResponse ping = client.execute(httppost);
+		//Test if error occured
+		if(ping.getStatusLine().getStatusCode()>=400){
+			throw new Exception("QAKIS Server could not answer due to: "+ping.getStatusLine());
+		}
+		
+		Document vsdoc = Jsoup.parse(responseparser.responseToString(ping));
+		Elements el = vsdoc.select("input");
+		String viewstate = (el.get(el.size() - 1).attr("value"));
 
-            Document vsdoc = Jsoup.parse(responseparser.responseToString(ping));
-            Elements el = vsdoc.select("input");
-            String viewstate = (el.get(el.size() - 1).attr("value"));
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("index_form", "index_form"));
+		formparams.add(new BasicNameValuePair("index_form:question",
+				questionString));
+		formparams.add(new BasicNameValuePair("index_form:eps", ""));
+		formparams.add(new BasicNameValuePair("index_form:submitQuestion", ""));
+		formparams.add(new BasicNameValuePair("javax.faces.ViewState",
+				viewstate));
 
-            List<NameValuePair> formparams = new ArrayList<NameValuePair>();
-            formparams.add(new BasicNameValuePair("index_form", "index_form"));
-            formparams.add(new BasicNameValuePair("index_form:question", questionString));
-            formparams.add(new BasicNameValuePair("index_form:eps", ""));
-            formparams.add(new BasicNameValuePair("index_form:submitQuestion", ""));
-            formparams.add(new BasicNameValuePair("javax.faces.ViewState", viewstate));
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams,
+				Consts.UTF_8);
+		httppost.setEntity(entity);
+		HttpResponse response = client.execute(httppost);
 
-            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
-            httppost.setEntity(entity);
-            HttpResponse response = client.execute(httppost);
+		Document doc = Jsoup.parse(responseparser.responseToString(response));
+		Elements answer = doc.select("div.global-presentation-details>h3>a");
+		NodeVisitor nv = new NodeVisitor() {
+			public void tail(Node node, int depth) {
+				if (depth == 0)
+					resultSet.add(node.attr("href"));
+			}
 
-            Document doc = Jsoup.parse(responseparser.responseToString(response));
-            Elements answer = doc.select("div.global-presentation-details>h3>a");
-            NodeVisitor nv = new NodeVisitor() {
-                public void tail(Node node, int depth) {
-                    if (depth == 0)
-                        resultSet.add(node.attr("href"));
-                }
+			public void head(Node arg0, int arg1) {
+				// do nothing here
+			}
+		};
+		answer.traverse(nv);
+		question.setGoldenAnswers(resultSet);
 
-                public void head(Node arg0, int arg1) {
-                    // do nothing here
-                }
-            };
-            answer.traverse(nv);
-            question.setGoldenAnswers(resultSet);
-
-            Elements codeElements = doc.select("div#sparqlQuery pre");
-            if (codeElements.size() > 0) {
-                Element sparqlElement = codeElements.get(0);
-                Elements codeChildren = sparqlElement.children();
-                for (Element c : codeChildren) {
-                    c.remove();
-                }
-                question.setSparqlQuery(sparqlElement.text());
-            }
-        } catch (Exception e) {
-            log.error(e.getLocalizedMessage(), e);
-        }
-    }
+		Elements codeElements = doc.select("div#sparqlQuery pre");
+		if (codeElements.size() > 0) {
+			Element sparqlElement = codeElements.get(0);
+			Elements codeChildren = sparqlElement.children();
+			for (Element c : codeChildren) {
+				c.remove();
+			}
+			question.setSparqlQuery(sparqlElement.text());
+		}
+	}
 }
--- qa.systems/src/main/java/org/aksw/qa/systems/START.java
+++ qa.systems/src/main/java/org/aksw/qa/systems/START.java
@@ -1,7 +1,6 @@
 package org.aksw.qa.systems;
 
 import java.net.URI;
-
 import org.aksw.qa.commons.datastructure.IQuestion;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -15,28 +14,34 @@ import org.slf4j.LoggerFactory;
 
 public class START extends ASystem {
 	Logger log = LoggerFactory.getLogger(START.class);
-	public String name(){return "start";};
 
-	public void search(IQuestion question) {
-        String questionString;
-        if (!question.getLanguageToQuestion().containsKey("en")) {
-            return;
-        }
-        questionString = question.getLanguageToQuestion().get("en");
-        log.debug(this.getClass().getSimpleName() + ": " + questionString);
-		try {
-			HttpClient client = HttpClientBuilder.create().build();
-			URI uri = new URIBuilder().setScheme("http").setHost("start.csail.mit.edu").setPath("/justanswer.php").setParameter("query", questionString).build();
-			HttpGet httpget = new HttpGet(uri);
-			HttpResponse response = client.execute(httpget);
+	public String name() {
+		return "start";
+	};
 
-			Document doc = Jsoup.parse(responseparser.responseToString(response));
-			System.out.println(doc.select("span[type=reply]").text());
-
-			// TODO return senseful answer from start
-			// return resultSet;
-		} catch (Exception e) {
-			log.error(e.getLocalizedMessage(), e);
+	public void search(IQuestion question) throws Exception {
+		String questionString;
+		if (!question.getLanguageToQuestion().containsKey("en")) {
+			return;
 		}
+		questionString = question.getLanguageToQuestion().get("en");
+		log.debug(this.getClass().getSimpleName() + ": " + questionString);
+
+		HttpClient client = HttpClientBuilder.create().build();
+		URI uri = new URIBuilder().setScheme("http")
+				.setHost("start.csail.mit.edu").setPath("/justanswer.php")
+				.setParameter("query", questionString).build();
+		HttpGet httpget = new HttpGet(uri);
+		HttpResponse response = client.execute(httpget);
+		//Test if error occured
+		if(response.getStatusLine().getStatusCode()>=400){
+			throw new Exception("START Server could not answer due to: "+response.getStatusLine());
+		}
+		
+		Document doc = Jsoup.parse(responseparser.responseToString(response));
+		System.out.println(doc.select("span[type=reply]").text());
+
+		// TODO return senseful answer from start
+		// return resultSet;
 	}
 }
