0e8aefcd4540270d4355cfcb19d8c1da900cd66c
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2017-12-18 17:15:22 +0100

    clean up class

--- qa.hawk/src/main/java/org/aksw/hawk/webservice/GerbilQABenchmarking.java
+++ qa.hawk/src/main/java/org/aksw/hawk/webservice/GerbilQABenchmarking.java
@@ -1,37 +1,22 @@
 package org.aksw.hawk.webservice;
 
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.aksw.hawk.controller.PipelineStanford;
+import org.aksw.hawk.datastructures.Answer;
 import org.aksw.hawk.datastructures.HAWKQuestion;
 import org.aksw.hawk.util.GerbilFinalResponse;
-import org.aksw.hawk.util.GerbilResponseBuilder;
-import org.aksw.qa.commons.load.json.*;
-import org.apache.commons.math3.geometry.Vector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.SimpleIdGenerator;
-import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,21 +24,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import com.google.common.collect.Maps;
+
 
 @RestController
 @Component
 @SpringBootApplication
-//@EnableAsync
 public class GerbilQABenchmarking{
 	private Logger log = LoggerFactory.getLogger(GerbilQABenchmarking.class);
 	private PipelineStanford pipeline = new PipelineStanford();
 	
 	
-	@Qualifier("SearchExecutor")
-	private SearchExecutor searchExecutor = new SearchExecutor();
-	
-	
+	//test via
+	// curl -d "query=Who is the president of Europe?&lang=en" -X POST http://localhost:8181/ask-gerbil
 	@RequestMapping(value = "/ask-gerbil", method = RequestMethod.POST)
 	public String askGerbil(@RequestParam Map<String,String> params, final HttpServletResponse response) throws ExecutionException, RuntimeException, IOException {
 		log.debug("Received question = " + params.get("query"));
@@ -67,18 +49,18 @@ public class GerbilQABenchmarking{
 		String question = params.get("query");
 		String lang = params.get("lang");
 		
-		HAWKQuestion answer = searchExecutor.runPipeline(question);
+		HAWKQuestion q = new HAWKQuestion();
+		q.getLanguageToQuestion().put("en", question);
+		List<Answer> answer = pipeline.getAnswersToQuestion(q);
+		q.setFinalAnswer(answer);
 		
 		GerbilFinalResponse resp = new GerbilFinalResponse();
-		resp.setQuestions(answer);
-		log.info("resp value: " + resp);
+		resp.setQuestions(q);
 		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 		String json = ow.writeValueAsString(resp);
 		
 		log.info("\n\n JSON object: \n\n" + json);
 
-		//ExtendedQALDJSONLoader.writeJson(resp, new File("./from_qald_to_extended_to_question_to_qald.json"), true);
-		log.info("Final Answer in object: " +answer.getFinalAnswer() + " Answertype: " + answer.getAnswerType() + " Sparql query: " + answer.getSparqlQuery("en"));
 		return json;
 	}
 	
