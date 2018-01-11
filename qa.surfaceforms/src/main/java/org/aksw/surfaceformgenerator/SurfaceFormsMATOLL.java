bd97a4707387d8982bd1612fcbc989c8ec05b447
Ricardo Usbeck <ricardo.usbeck@googlemail.com>
2018-01-11 15:18:52 +0100

    matoll improved

--- qa.surfaceforms/src/main/java/org/aksw/surfaceformgenerator/SurfaceFormsMATOLL.java
+++ qa.surfaceforms/src/main/java/org/aksw/surfaceformgenerator/SurfaceFormsMATOLL.java
@@ -1,54 +1,39 @@
 package org.aksw.surfaceformgenerator;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 public class SurfaceFormsMATOLL {
 	public static String DIRECTORY = "data/2016-10/en/";
 	public static String DBPEDIA_MATOLL_FILE = "" + DIRECTORY + "matoll_EN" + ".ttl";
-	public static String SURFACE_FORMS = "" + DIRECTORY + "_surface_forms.ttl";
+	public static String SURFACE_FORMS = "" + DIRECTORY + "matoll_EN_surface_forms.ttl";
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-		SURFACE_FORMS = DIRECTORY + "surface_forms.ttl";
-
-		HashMap<String, Set<String>> hmap = new HashMap<>();
-		Set tempSet = new HashSet<String>();
-		String values = null;
 
 		FileReader in = new FileReader(DBPEDIA_MATOLL_FILE);
 		BufferedReader br = new BufferedReader(in);
+		BufferedWriter bw = new BufferedWriter(new FileWriter(SURFACE_FORMS));
 		while (br.ready()) {
 			String readLine = br.readLine();
 			String[] split = readLine.split("\t");
-			String ar = "<" + split[4] + "> rdfs:label \"" + split[0] + "\" .";
-			String str = "<http://dbpedia.org/property/";
-			if (ar.contains(str)) {
-
-				String[] property = ar.split(" ");
-				String p = property[0];
-				String l = property[2];
-				if (!hmap.containsValue(p)) {
-					tempSet.add(l);
-					hmap.put(p, tempSet);
-				} else {
-					tempSet = hmap.get(l);
-					tempSet.add(p);
-					hmap.put(l, tempSet);
-				}
+			String label = split[0];
+			String prep = split[1];
+			String wordtype = split[2];
+			String propertyURI = split[4];
+			String namespace = "http://dbpedia.org/ontology/";
+			if (wordtype.equals("verb") && propertyURI.startsWith(namespace)) {
+				String ar = "<" + propertyURI + "> <http://www.w3.org/2000/01/rdf-schema#label> \"" + label + " " + prep + "\" .";
+				System.out.println(ar);
+				bw.write(ar);
+				bw.newLine();
 			}
 		}
-
-		for (HashMap.Entry<String, Set<String>> entry : hmap.entrySet()) {
-
-			values = entry.getKey() + entry.getValue();
-			System.out.println(values);
-
-		}
+		br.close();
+		bw.close();
 
 	}
 }
