f1259f7e3d1d0410a148c8dc1fdd55136b68caa3
datascienceadmin <rrichajalota@gmail.com>
2018-01-25 16:50:41 +0100

    missing links added

--- qa.hawk/deploy-scripts/old-dbpedia-index_fuseki.sh
+++ qa.hawk/deploy-scripts/old-dbpedia-index_fuseki.sh
@@ -43,12 +43,12 @@ then
 fi
 if [ ! -f "mappingbased_literals_en.ttl" ];
 then
-  wget http://downloads.dbpedia.org/3.9/en/mappingbased_literals_en.ttl.bz2
+  wget http://downloads.dbpedia.org/2015-10/core-i18n/en/mappingbased_literals_en.ttl.bz2
   bunzip2 mappingbased_literals_en.ttl.bz2
 fi
 if [ ! -f "mappingbased_objects_en.ttl" ];
 then
-  wget http://downloads.dbpedia.org/3.9/en/mappingbased_objects_en.ttl.bz2
+  wget http://downloads.dbpedia.org/2015-10/core-i18n/en/mappingbased_objects_en.ttl.bz2
   bunzip2 mappingbased_objects_en.ttl.bz2
 fi
 if [ ! -f "persondata_en.ttl" ];
@@ -58,17 +58,17 @@ then
 fi
 if [ ! -f "specific_mappingbased_properties_en.ttl" ];
 then
-  wget http://downloads.dbpedia.org/3.9/en/specific_mappingbased_properties_en.ttl.bz2
+  wget http://downloads.dbpedia.org/2015-10/core-i18n/en/specific_mappingbased_properties_en.ttl.bz2
   bunzip2 specific_mappingbased_properties_en.ttl.bz2
 fi
 if [ ! -f "transitive_redirects_en.ttl" ];
 then
-  wget http://downloads.dbpedia.org/3.9/en/transitive_redirects_en.ttl.bz2
+  wget http://downloads.dbpedia.org/2015-10/core-i18n/en/transitive_redirects_en.ttl.bz2
   bunzip2 transitive_redirects_en.ttl.bz2
 fi
 if [ ! -f "instance_types_transitive_en.ttl" ];
 then
-  wget http://downloads.dbpedia.org/3.9/en/instance_types_transitive_en.ttl.bz2
+  wget http://downloads.dbpedia.org/2015-10/core-i18n/en/instance_types_transitive_en.ttl.bz2
   bunzip2 instance_types_transitive_en.ttl.bz2
 fi
 
