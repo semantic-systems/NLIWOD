96eaad93bee3d807d6f6080736578f2418a762b0
nikit91@gmail.com <nikit91@gmail.com>
2018-04-09 08:18:18 +0200

    Gerbil Issue 238: Correcting the name to gAnswer2.
    https://github.com/dice-group/gerbil/pull/248#issuecomment-379381029

--- qa.systems/src/main/java/org/aksw/qa/systems/GANSWER2.java
+++ qa.systems/src/main/java/org/aksw/qa/systems/GANSWER2.java
@@ -2,6 +2,6 @@ package org.aksw.qa.systems;
 
 public class GANSWER2 extends Gen_HTTP_QA_Sys{
 	public GANSWER2( String url) {
-		super(url, "ganswer2", false, false);
+		super(url, "gAnswer2", false, false);
 	}
 }
